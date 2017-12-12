66657d8e791e1799483f5148b3e6e675a2f491d3
DrMonkey <Adriantomasv@gmail.com>
2017-12-12 16:17:55 +0100

    Switched mariasql for mysql

--- api/config/connection.js
+++ api/config/connection.js
@@ -1,11 +1,18 @@
 //No cambiar los valores bajo ningun concepto
-var mariadb = require('mariasql');
-var connection = new mariadb({
+var mysql = require('mysql');
+var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
 	password: 'gardiot',
 	charset: 'utf8',
-	db: 'gardiotDB'
+	database: 'gardiotDB'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
 });
 
 module.exports = connection;
