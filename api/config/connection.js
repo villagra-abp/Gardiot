22c7d1fe4aa187efa3f4ec1e0309806a1ba3583e
Mariano <mariano.lopez@convershare.com>
2017-12-19 16:45:30 +0100

    para no putear sorry

--- api/config/connection.js
+++ api/config/connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
-	password: '',
+	password: 'gardior',
 	charset: 'utf8',
 	database: 'gardiotDB'
 });
