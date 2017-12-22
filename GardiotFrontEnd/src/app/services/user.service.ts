abec5d6195264e9107713f52eb5a0822bcbf1e66
Luis Berenguer Herrero <luisberenguer96@gmail.com>
2017-12-22 11:16:15 +0100

    DistV2

--- GardiotFrontEnd/src/app/services/user.service.ts
+++ GardiotFrontEnd/src/app/services/user.service.ts
@@ -7,7 +7,7 @@ import 'rxjs/Rx';
 @Injectable()
 export class UserService {
 
-  private apiURL:string="http://localhost:3000/api/";
+  private apiURL:string="https://gardiot.ovh:3000/api/";
 
   constructor( private http:Http, private _route:Router) {}
 
