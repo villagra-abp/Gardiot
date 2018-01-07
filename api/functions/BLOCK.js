2c48529d68f08c7f0b1ec8f2adc43fdc12f05681
DrMonkey <Adriantomasv@gmail.com>
2018-01-07 19:16:20 +0100

    Miniblockfix

--- api/functions/BLOCK.js
+++ api/functions/BLOCK.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 module.exports = router.all('*', function (req, res, next) {
 	if (req.method === 'GET' || req.baseUrl === '/user' || req.baseUrl === '/authenticate' || req.baseUrl === '/login' || req.baseUrl === '/logout')
-		return next();
+		next();
 	else
 		res.status(501).json({"Mensaje":"Ruta no disponible por el momento"});
 });
