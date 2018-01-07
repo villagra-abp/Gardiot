ad5f73aabe3067893ec9845050a9e7fa2bedcbc9
DrMonkey <Adriantomasv@gmail.com>
2018-01-07 18:55:34 +0100

    Possible fix

--- api/functions/BLOCK.js
+++ api/functions/BLOCK.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
 module.exports = router.all('*', function (req, res, next) {
-  res.status(501).json({"Mensaje":"Ruta no disponible por el momento"});
+	if (req.method === 'GET' || req.baseUrl === '/user' || req.baseUrl === '/authenticate' || req.baseUrl === '/login' || req.baseUrl === '/logout')
+		return next();
+	else
+		res.status(501).json({"Mensaje":"Ruta no disponible por el momento"});
 });
