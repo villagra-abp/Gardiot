d98d3c1aaa74fded6d66aa1282de2e9f2af7befa
DrMonkey <Adriantomasv@gmail.com>
2018-01-08 18:08:52 +0100

    Block fixes

--- api/functions/BLOCK.js
+++ api/functions/BLOCK.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 module.exports = router.all('*', function (req, res, next) {
-	if (req.method === 'GET' || req.baseUrl === '/user' || req.baseUrl === '/authenticate' || req.baseUrl === '/login' || req.baseUrl === '/logout')
+	if (req.method == 'GET' || req.path == '/user' || req.path == '/authenticate' || req.path == '/login' || req.path == '/logout' || req.path == '/register')
 		next();
 	else
 		res.status(501).json({"Mensaje":"Ruta no disponible por el momento"});
