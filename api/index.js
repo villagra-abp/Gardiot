71d9fd8066ed9d9041b7c9d31e062e554ca4735c
DrMonkey <Adriantomasv@gmail.com>
2018-01-16 19:24:36 +0100

    Configured OPTIONS for CORS request

--- api/index.js
+++ api/index.js
@@ -15,6 +15,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cors());
+app.options('*', cors());
 app.use(helmet());
 app.use(morgan('dev'));
 
