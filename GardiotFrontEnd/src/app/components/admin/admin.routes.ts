c84fbe18939e2f2ec6bd0af2406b1813903158c0
Mariano <sr.rizado@gmail.com>
2018-01-22 18:24:44 +0100

    MapWeb del admin

--- GardiotFrontEnd/src/app/components/admin/admin.routes.ts
+++ GardiotFrontEnd/src/app/components/admin/admin.routes.ts
@@ -8,6 +8,15 @@ import { AdminComponent } from './admin.component';
 import { AdminEditUserComponent } from './edituser.component';
 import { AdminDashboardComponent } from './dashboard.component';
 
+import { StatisticsComponent } from './statistics.component';
+import { AnalyticsComponent } from './analytics.component';
+import { EmailComponent } from './email.component';
+import { UserdataComponent } from './userdata.component';
+import { PlantdataComponent } from './plantdata.component';
+import { EventdataComponent } from './eventdata.component';
+import { ToolComponent } from './tool.component';
+import { InvoiceComponent } from './invoice.component';
+
 import { AdminguardGuard } from "./adminguard.guard";
 
 
@@ -16,6 +25,15 @@ export const admin_routes: Routes = [
   { path: 'user', component: AdminUserComponent, canActivate: [AdminguardGuard] },
   { path: 'listusers', component: AdminListUsersComponent, canActivate: [AdminguardGuard] },
   { path: 'edituser/:id', component: AdminEditUserComponent, canActivate: [AdminguardGuard] },
+
+  { path: 'analytics', component: AnalyticsComponent, canActivate: [AdminguardGuard] },
+  { path: 'statistics', component: StatisticsComponent, canActivate: [AdminguardGuard] },
+  { path: 'email', component: EmailComponent, canActivate: [AdminguardGuard] },
+  { path: 'userdata', component: UserdataComponent, canActivate: [AdminguardGuard] },
+  { path: 'plantdata', component: PlantdataComponent, canActivate: [AdminguardGuard] },
+  { path: 'eventdata', component: EventdataComponent, canActivate: [AdminguardGuard] },
+  { path: 'tool', component: ToolComponent, canActivate: [AdminguardGuard] },
+  { path: 'invoice', component: InvoiceComponent, canActivate: [AdminguardGuard] },
 
   { path: '**', pathMatch: 'full', redirectTo: 'dashboard' }
 ];
---
+++ GardiotFrontEnd/src/app/components/admin/email.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-email',
+  templateUrl: './email.component.html'
+
+})
+export class EmailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
