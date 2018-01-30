c4b6dc052bf72b50534a13c1bb6886e68f1eae84
DrMonkey <Adriantomasv@gmail.com>
2018-01-30 16:10:21 +0100

    An small fix

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
-var cors = require('cors'); //CORS standard
-var corsOptions = require('../config/cors');
+//var cors = require('cors'); //CORS standard
+//var corsOptions = require('../config/cors');
 
-router.options('/auth/google', cors(corsOptions));
-router.get('/auth/google', cors(corsOptions), passport.authenticate('google', {scope: ['profile', 'email'], prompt:'consent', session: false}));
+//router.options('/auth/google', cors(corsOptions));
+//router.get('/auth/google',  /**cors(corsOptions),**/  passport.authenticate('google', {scope: ['profile', 'email'], prompt:'consent', session: false}));
+router.get('/auth/google',  passport.authenticate('google', {scope: ['profile', 'email'], prompt:'consent', session: false}));
 
-router.options('/auth/google/callback', cors(corsOptions));
-router.get('/auth/google/callback', cors(corsOptions), passport.authenticate('google', { failureRedirect: '/login', session: false }), function(request, response) {
+//router.options('/auth/google/callback', cors(corsOptions));
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login', session: false }), function(request, response) {
     if (request.user.token) 
     	response.json({"Token": request.user.token});
     else
