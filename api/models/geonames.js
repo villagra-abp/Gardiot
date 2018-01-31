1fd098615291993a1b079360a9ee330487c83cb8
Marco <gonzalez.marco95@gmail.com>
2018-01-31 18:49:49 +0100

    added more routes of geonames

--- api/models/geonames.js
+++ api/models/geonames.js
@@ -4,9 +4,23 @@ var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var geonames = {};
 
 
-geonames.getByZip = function(zip, callback) {
+geonames.getByZip = function(zip, code, callback) {
   var req = new XMLHttpRequest();
-  req.open('GET', 'http://api.geonames.org/postalCodeSearchJSON?postalcode='+zip+'&username=escaoba', false);
+  req.open('GET', 'http://api.geonames.org/postalCodeSearchJSON?postalcode='+zip+'&country='+ code+'&username=escaoba', false);
+  req.send(null);
+    callback(null, req.responseText);
+}
+
+geonames.getAllCountries = function(callback) {
+  var req = new XMLHttpRequest();
+  req.open('GET', 'http://api.geonames.org/countryInfoJSON?username=escaoba', false);
+  req.send(null);
+    callback(null, req.responseText);
+}
+
+geonames.getCities= function(code, callback) {
+  var req = new XMLHttpRequest();
+  req.open('GET', 'http://api.geonames.org/searchJSON?country='+ code+'&username=escaoba', false);
   req.send(null);
     callback(null, req.responseText);
 }
