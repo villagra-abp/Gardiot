0cb1161dd3f1069ae4d310d29ef5066fb3a8006d
DrMonkey <Adriantomasv@gmail.com>
2018-01-31 13:54:14 +0100

    Removed cors from all routes except user routes

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
-var cors = require('cors'); //CORS standard
-var corsOptions = require('../config/cors');
+//var cors = require('cors'); //CORS standard
+//var corsOptions = require('../config/cors');
 
 /**router.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
@@ -10,19 +10,20 @@ var corsOptions = require('../config/cors');
   	next();
 });**/
 
-router.options('/auth/google', cors(corsOptions), function(request, response, next) {
+/*router.options('/auth/google', cors(corsOptions), function(request, response, next) {
 	console.log("ESTOY ENTRANDO AL OPTIONS");
 	next();
-});
+});*/
 
-router.get('/auth/google',  cors(corsOptions),  passport.authenticate('google', {scope: ['profile', 'email'], prompt:'consent', session: false}));
-//router.get('/auth/google',  passport.authenticate('google', {scope: ['profile', 'email'], prompt:'consent', session: false}));
+//router.get('/auth/google',  cors(corsOptions),  passport.authenticate('google', {scope: ['profile', 'email'], prompt:'consent', session: false}));
+router.get('/auth/google',  passport.authenticate('google', {scope: ['profile', 'email'], prompt:'consent', session: false}));
 
-router.options('/auth/google/callback', cors(corsOptions), function(request, response, next) {
+/*router.options('/auth/google/callback', cors(corsOptions), function(request, response, next) {
 	console.log("ESTOY ENTRANDO AL OPTIONS DEL CALLBACK");
 	next();
-});
-router.get('/auth/google/callback', cors(corsOptions), passport.authenticate('google', { failureRedirect: '/login', session: false }), function(request, response) {
+});*/
+
+router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login', session: false }), function(request, response) {
     if (request.user.token) 
     	response.json({"Token": request.user.token});
     else
