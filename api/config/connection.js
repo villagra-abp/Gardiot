747d9d6e515faee8f7c05a6a443a933b9536a913
Mariano <sr.rizado@gmail.com>
2018-02-01 12:23:04 +0100

    Oauth funcional token
    falta resolver el inicio de sessión que será el siguiente paso

--- api/config/connection.js
+++ api/config/connection.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 var db_config = {
 	host: 'localhost',
 	user: 'root',
-	password: 'gardiot',
+	password: '',
 	charset: 'utf8',
 	database: 'gardiotDB'
 };
@@ -20,21 +20,21 @@ var connection;
 
 
 function handleDisconnect() {
-  connection = mysql.createConnection(db_config); 
+  connection = mysql.createConnection(db_config);
 
-  connection.connect(function(err) {             
-    if(err) {                                     
+  connection.connect(function(err) {
+    if(err) {
       console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); 
-    }                                     
-  });                                     
-                                          
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+
   connection.on('error', function(err) {
     console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
-      handleDisconnect();                         
-    } else {                                      
-      throw err;                                  
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
     }
   });
 }
