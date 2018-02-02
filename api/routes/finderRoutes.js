c56b5ad7ed6389240056da63c4998def466a2ceb
DrMonkey <Adriantomasv@gmail.com>
2018-02-02 14:16:19 +0100

    Added generic finder

--- api/routes/finderRoutes.js
+++ api/routes/finderRoutes.js
@@ -1,30 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
+var validator = require('validator');
 
 var requireAdmin = require('../functions/adminCheck');
 var requireActive = require('../functions/userActiveCheck');
 var requireActiveToken = require('../functions/tokenCheck');
 
-router.post('/find/:model', /*passport.authenticate('jwt', {session: false}), requireActive, requireActiveToken,*/ function(request, response) {
+var finderModel = require('../models/finder');
+
+router.post('/find/:model', passport.authenticate('jwt', {session: false}), requireActive, requireActiveToken, function(request, response) {
 	if (!request.params.model)
 		response.status(400).json({"Mensaje":"Petición errónea."}); 
-	else {
-		if (request.params.model == 'user') {
-			var userData = {
-				id: request.body.id,
-				name: request.body.name,
-				birthDate: request.body.birthDate,
-				city: request.body.city,
-				plan: request.body.plan
-			};
-		}
-		else if (request.params.model == 'category') {
-
-		}
-		else 
-			response.status(400).json({"Mensaje":"Modelo incorrecto"});
+	else {		
+		var body = request.body;
+		var data = {};
+		for (var key in body) 
+			data[key] = validator.escape(validator.trim(body[key]));
+			
+		finderModel.find(request.params.model, data, function(error, data) {
+			if (data)
+				response.status(200).json(data);
+			else
+				response.status(400).json({"Mensaje":"Error al buscar los datos."});
+		});
 	}
 });
+
 
 module.exports = router;
