d97fcb676a8d17df9eec32f814715c116da82bfe
DrMonkey <Adriantomasv@gmail.com>
2018-02-13 16:53:54 +0100

    Tested and working

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -134,7 +134,8 @@ router.get('/isAuthenticated', function(request, response) {
 //***Muestra al usuario actual. Sin parametros
 
 router.get('/user', passport.authenticate('jwt', {session: false}), routeRequirements, function(request, response) {
-	response.status(200).json({"Name:":request.user.name, "LastName":request.user.lastName, "Photo":request.user.photo}); //PASSPORT devuelve siempre el objeto user
+	response.status(200).json(request.user); //Arreglar
+	//response.status(200).json({"Name:":request.user.name, "LastName":request.user.lastName, "Photo":request.user.photo}); //PASSPORT devuelve siempre el objeto user
 });
 
 
