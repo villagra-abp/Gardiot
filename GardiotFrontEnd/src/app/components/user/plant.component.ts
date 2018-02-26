83c876be824d6b0b5fc1dc17c859995187f74287
Julián Sánchez <eljovenjuli@hotmail.com>
2018-02-26 20:58:19 +0100

    PlantFixMonthDateOK

--- GardiotFrontEnd/src/app/components/user/plant.component.ts
+++ GardiotFrontEnd/src/app/components/user/plant.component.ts
@@ -13,7 +13,19 @@ export class PlantComponent implements OnInit {
 
   plant = new Plant("");
 
+  iniSiembra:String;
+  finSiembra:String;
+  iniFlores:String;
+  finFlores:String;
+  iniRecolectar:String;
+  finRecolectar:String;
+
+  mes:String;
+
+
   constructor(
+
+
     private _plantService:PlantService) {}
 
       mostrar(){
@@ -28,17 +40,21 @@ export class PlantComponent implements OnInit {
               this.plant.family=data[0].family;
               this.plant.depth=data[0].depth;
               this.plant.distance=data[0].distance;
-
               this.plant.diseaseResist=data[0].diseaseResist;
 
-              this.plant.initDatePlant=data[0].initDatePlant;
-              this.plant.finDatePlant=data[0].finDatePlant;
 
-              this.plant.initDateBloom=data[0].initDateBloom;
-              this.plant.finDateBloom=data[0].finDateBloom;
-
-              this.plant.initDateHarvest=data[0].initDateHarvest;
-              this.plant.finDateHarvest=data[0].finDateHarvest;
+              //this.plant.initDatePlant=data[0].initDatePlant;
+              this.iniSiembra=this.dameMes(data[0].initDatePlant);
+              //this.plant.finDatePlant=data[0].finDatePlant;
+              this.finSiembra=this.dameMes(data[0].finDatePlant);
+              //this.plant.initDateBloom=data[0].initDateBloom;
+              this.iniFlores=this.dameMes(data[0].initDateBloom);
+              //this.plant.finDateBloom=data[0].finDateBloom;
+              this.finFlores=this.dameMes(data[0].finDateBloom);
+              //this.plant.initDateHarvest=data[0].initDateHarvest;
+              this.iniRecolectar=this.dameMes(data[0].initDateHarvest);
+              //this.plant.finDateHarvest=data[0].finDateHarvest;
+              this.finRecolectar=this.dameMes(data[0].finDateHarvest);
 
               this.plant.leaveType=data[0].leaveType;
               // this.plant.commonName=data[0].3DModel;
@@ -50,6 +66,40 @@ export class PlantComponent implements OnInit {
 
           });
 
+    }
+    dameMes(fechas){
+      var fecha = fechas;
+      var array = fecha.toString();
+      var arrayRoto = array.split("-");
+      this.mes = arrayRoto[1];
+
+      switch(this.mes){
+        case '01': this.mes="Enero";
+        break;
+        case '02': this.mes="Febrero";
+        break;
+        case '03': this.mes="Marzo";
+        break;
+        case '04': this.mes="Abril";
+        break;
+        case '05': this.mes="Mayo";
+        break;
+        case '06': this.mes="Junio";
+        break;
+        case '07': this.mes="Julio";
+        break;
+        case '08': this.mes="Agosto";
+        break;
+        case '09': this.mes="Septiembre";
+        break;
+        case '10': this.mes="Octubre";
+        break;
+        case '11': this.mes="Noviembre";
+        break;
+        case '12': this.mes="Diciembre";
+        break;
+      }
+      return this.mes;
     }
 
 
