f60f1d63c74d9071fd7f2a72aeb976d903b4728c
DrMonkey <Adriantomasv@gmail.com>
2018-03-05 18:46:20 +0100

    Added MyPlant, fixed stuff

--- api/routes/myPlantRoutes.js
+++ api/routes/myPlantRoutes.js
@@ -1,84 +1,159 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var validator = require('validator');
+var routeRequirements = require('../functions/routeRequirements');
 
 var myPlantModel = require('../models/myPlant');
 
-router.get('/myPlant', function (request, response) {
-  myPlantModel.getMyPlant (function(error, data){
-    response.status(200).json(data);
-  });
+router.get('/myPlants/:garden', passport.authenticate('jwt', {session: false}), routeRequirements, function (request, response) {
+	if (!validator.isInt(request.params.garden, {gt: 0}))
+		response.status(400).json({"Mensaje":"Petición incorrecta"});
+	else {
+  		myPlantModel.getMyPlantsByGarden (request.params.garden, request.user.id, function(error, data){
+  			if (data)
+    			response.status(200).json(data);
+    		else
+    			response.status(500).json({"Mensaje":"Petición incorrecta"});
+  		});
+  	}
 });
 
-router.get('/myPlant/:id', function(request, response) {
-	var id = request.params.id;
-	myPlantModel.getMyPlantById(id, function(error, data) {
-		if (typeof data !== 'undefined' && data.length > 0) {
-			response.status(200).json(data);
-		}
-		else {
-			response.status(404).json({"Mensaje":"No existe"});
-		}
-	});
+router.get('/myPlant/:garden/:id', passport.authenticate('jwt', {session: false}), routeRequirements, function(request, response) {
+	if (!validator.isInt(request.params.garden, {gt: 0}) || !validator.isInt(request.params.id, {gt: 0}))
+		response.status(400).json({"Mensaje":"Petición incorrecta"});
+	else {
+		myPlantModel.getMyPlantById(request.params.garden, request.user.id, request.params.id, function(error, data) {
+			if (typeof data !== 'undefined') 
+				response.status(200).json(data);
+			else 
+				response.status(404).json({"Mensaje":"No existe"});
+		});
+	}	
 });
 
-router.post('/myPlant', function(request, response) {
-	var myPlantData = {
-		name: request.body.name,
-		xCoordinate: request.body.xCoordinate,
-    yCoordinate: request.body.yCoordinate,
-    seed: request.body.seed,
-    number: request.body.number,
-    plant: request.body.plant,
-    garden: request.body.garden,
-    soil: request.body.soil,
-	};
-
-	myPlantModel.insertMyPlant(myPlantData, function(error, data) {
-		if (data) {
-			response.status(200).json({"Mensaje":"Insertado"});
-		}
-		else {
-			response.status(500).json({"Mensaje":"Error"});
-		}
-	});
+router.post('/myPlant/:garden', passport.authenticate('jwt', {session: false}), routeRequirements, function(request, response) {
+	if (!request.body.name || !request.body.seed || !request.body.number || !request.body.plant || !request.body.soil)
+		response.status(400).json({"Mensaje":"Faltan parámetros necesarios"});
+	else if (!validator.isInt(request.params.garden, {gt: 0}))
+		response.status(400).json({"Mensaje":"Petición incorrecta"});
+	else {		
+		myPlantModel.isOwner(request.user.id, request.params.garden, function (error, owner) {
+			if (error)
+				response.status(400).json({"Mensaje":"Error: " + error.message});
+			else {
+				if (owner == true) {
+					var myPlantData = {
+						name: request.body.name,
+						xCoordinate: request.body.xCoordinate,
+					    yCoordinate: request.body.yCoordinate,
+					    seed: request.body.seed,
+					    number: request.body.number,
+					    plant: request.body.plant,
+					    soil: request.body.soil
+					};
+					var validate = validateInput(myPlantData);
+					if (validate.length > 0)
+						response.status(400).json({"Mensaje": validate});
+					else {
+						myPlantData = sanitizeInput(myPlantData);
+						myPlantModel.insertMyPlant(request.params.garden, myPlantData, function(error, data) {
+							if (data == 1) 
+								response.status(200).json({"Mensaje":"Insertado"});			
+							else 
+								response.status(500).json({"Mensaje":"Error"});			
+						});
+					}
+				}
+				else response.status(403).json({"Mensaje":"No puedes insertar una planta en el jardin de otro usuario."});
+			}
+		});		
+	}	
 });
 
-router.put('/myPlant', function(request, response) {
-	var myPlantData = {
-		id: request.body.id,
-    name: request.body.name,
-		xCoordinate: request.body.xCoordinate,
-    yCoordinate: request.body.yCoordinate,
-    seed: request.body.seed,
-    number: request.body.number,
-    plant: request.body.plant,
-    garden: request.body.garden,
-    soil: request.body.soil,
-	};
-	
-	myPlantModel.updateMyPlant(myPlantData, function(error, data) {
-		if (data && data.mensaje) {
-			response.status(200).json(data);
-		}
-		else {
-			response.status(500).json({"Mensaje":"Error"});
-		}
-	});
+router.put('/myPlant/:garden/:id', passport.authenticate('jwt', {session: false}), routeRequirements, function(request, response) {
+	if (!validator.isInt(request.params.garden, {gt: 0}) || !validator.isInt(request.params.id, {gt: 0}))
+		response.status(400).json({"Mensaje":"Petición incorrecta"});
+	else {
+		myPlantModel.isOwner(request.user.id, request.params.garden, function (error, owner) {
+			if (error)
+				response.status(400).json({"Mensaje":"Error: " + error.message});
+			else {
+				if (owner == true) {
+					var myPlantData = {
+						name: request.body.name,
+						xCoordinate: request.body.xCoordinate,
+					    yCoordinate: request.body.yCoordinate,
+					    seed: request.body.seed,
+					    number: request.body.number,
+					    plant: request.body.plant,
+					    soil: request.body.soil
+					};
+					var validate = validateInput(myPlantData);
+					if (validate.length > 0)
+						response.status(400).json({"Mensaje": validate});
+					else {
+						myPlantData = sanitizeInput(myPlantData);
+						myPlantModel.updateMyPlant(request.params.id, myPlantData, function(error, data) {
+							if (data == 1) 
+								response.status(200).json({"Mensaje":"Actualizado"});			
+							else 
+								response.status(500).json({"Mensaje":"Error"});			
+						});
+					}				
+				}
+				else response.status(403).json({"Mensaje":"No puedes modificar una planta en el jardin de otro usuario."});
+			}
+		});	
+	}
 });
 
-router.delete('/myPlant/:id', function(request, response) {
-	var id = request.params.id;
-	myPlantModel.deleteMyPlant(id, function(error, data) {
-		if (data == 1) {
-			response.status(200).json({"Mensaje":"Borrado"});
-		}
-		else if (data == 0) {
-			response.status(404).json({"Mensaje":"No existe"});
-		}
-		else {
-			response.status(500).json({"Mensaje":"Error"});
-		}
-	});
+router.delete('/myPlant/:garden/:id', passport.authenticate('jwt', {session: false}), routeRequirements, function(request, response) {
+	if (!validator.isInt(request.params.garden, {gt: 0}) || !validator.isInt(request.params.id, {gt: 0}))
+		response.status(400).json({"Mensaje":"Petición incorrecta"});
+	else {
+		myPlantModel.isOwner(request.user.id, request.params.garden, function (error, owner) {
+			if (error)
+				response.status(400).json({"Mensaje":"Error: " + error.message});
+			else {
+				if (owner == true) {
+					myPlantModel.deleteMyPlant(id, function(error, data) {
+						if (data == 1) response.status(200).json({"Mensaje":"Borrado"});						
+						else if (data == 0) response.status(404).json({"Mensaje":"No existe"});						
+						else response.status(500).json({"Mensaje":"Error"});
+					});
+				}
+				else response.status(403).json({"Mensaje":"No puedes borrar una planta en el jardin de otro usuario."});
+			}
+		});
+	}
 });
+
+function sanitizeInput(data) {
+  if (data.id) {  data.id = validator.trim(data.id); data.id = validator.toInt(data.id);}
+  if (data.name) { data.name = validator.trim(data.name); data.name = validator.stripLow(data.name); data.name = validator.escape(data.name);}
+  if (data.xCoordinate) {  data.xCoordinate = validator.trim(data.xCoordinate); data.xCoordinate = validator.toInt(data.xCoordinate);}
+  if (data.yCoordinate) {  data.yCoordinate = validator.trim(data.yCoordinate); data.yCoordinate = validator.toInt(data.yCoordinate);}
+  if (data.number) {  data.number = validator.trim(data.number); data.number = validator.toInt(data.number);}  
+  if (data.plant) {  data.plant = validator.trim(data.plant); data.plant = validator.toInt(data.plant);}
+  if (data.garden) {  data.garden = validator.trim(data.garden); data.garden = validator.toInt(data.garden);}
+  if (data.soil) {  data.soil = validator.trim(data.soil); data.soil = validator.toInt(data.soil);}
+  return data;
+}
+
+function validateInput(data) {
+  var resp = '';
+  if (data.id && !validator.isInt(data.id)) resp += 'ID no válido, ';
+  if (data.name && !validator.isAscii(data.name)) resp += 'Nombre no válido, ';
+  if (data.xCoordinate && !validator.isInt(data.xCoordinate)) resp += 'Coordenada X no válida, ';
+  if (data.yCoordinate && !validator.isInt(data.yCoordinate)) resp += 'Coordenada Y no válida, ';
+  if (data.number && !validator.isInt(data.number)) resp += 'Número no válido, ';
+  if (data.plant && !validator.isInt(data.plant)) resp += 'Planta no válida, ';
+  if (data.garden && !validator.isInt(data.garden)) resp += 'Jardín no válido, ';
+  if (data.soil && !validator.isInt(data.soil)) resp += 'Suelo no válido, ';
+  if (data.seed && !validator.isISO8601(data.seed)) resp += 'Fecha no válida, ';
+  if (resp) resp = resp.slice(0, -2);
+  return resp;
+}
 
 module.exports = router;
