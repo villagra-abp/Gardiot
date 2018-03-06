d75e335e7dc56f2ce438608568268223f802121d
Luis Berenguer Herrero <luisberenguer96@gmail.com>
2018-03-06 18:38:52 +0100

    Angular&TAGWorks

--- GardiotFrontEnd/src/app/components/user/garden.component.ts
+++ GardiotFrontEnd/src/app/components/user/garden.component.ts
@@ -5,6 +5,9 @@ import { GardenService} from "../../services/garden.service";
 import { Garden } from "../../classes/garden.class";
 import { AppComponent } from "../../app.component";
 import { Observable } from 'rxjs/Observable';
+declare var mat4:any;
+declare var TGestorRecursos:any;
+declare var TMotor:any;
 
 @Component({
   selector: 'app-garden',
@@ -19,6 +22,9 @@ export class GardenComponent {
   presion ="Cargando";
   viento = "Cargando";
   angulo = "0";
+  canvas;
+  motor=new TMotor();
+  gestor=new TGestorRecursos();
 
   constructor(
   	private _gardenService:GardenService,
@@ -28,7 +34,7 @@ export class GardenComponent {
 
   ngOnInit() {
   	this.mostrar();
-
+    this.inicializar();
   }
 
 
@@ -55,9 +61,9 @@ export class GardenComponent {
         if(JSON.parse(error._body).Mensaje == 'No existe'){
           this._route.navigate(['/newgarden']);
         }else{
-          this._route.navigate(['/detail']);  
+          this._route.navigate(['/detail']);
         }
-        
+
       });
 
   }
@@ -67,14 +73,14 @@ export class GardenComponent {
 	        .subscribe(data=>{
 	          console.log(data);
 
-          
+
 		  		this.cielo = data.weather[0].main;
           var aux = data.main.temp - 273;
           this.temperatura = aux.toFixed(2);
           this.humedad = data.main.humidity;
           this.presion =  data.main.pressure;
           this.viento = data.wind.speed;
-          
+
 
 	        },
 	      error => {
@@ -84,4 +90,84 @@ export class GardenComponent {
 	        this._route.navigate(['/login']);
 	      });
   }
+
+  inicializar(){
+        this.canvas=null;
+        window.motor=this.motor;
+        window.gestor=this.gestor;
+        window.matrixProjection=[];
+        window.matrixStack=[];
+        window.matrixModel=mat4.create();
+
+        matrixStack.push(matrixModel);
+
+        //inicializamos vector de texturas
+        window.textureImage=[];
+        //variable que tendrá el programa webgl
+        window.gl=null;
+
+
+        this.motor.crearNodoLuz("luz1", false, undefined);
+
+        this.motor.crearNodoCamara("camara1", true, undefined);
+
+
+
+
+        this.motor.crearNodoMalla("malla2", "cubo", undefined);
+
+
+
+
+        this.motor.moverMalla("malla2", 0, -20, 0);
+        this.motor.rotarMalla("malla2", 45, "y");
+        this.motor.escalarMalla("malla2", 50, 1, 50);
+
+
+        this.motor.moverLuz("luz1", -10, 25, 7);
+
+
+
+
+
+
+        this.motor.moverCamara("camara1", 0, 5, 25);
+        this.motor.rotarCamara("camara1", -20, "x");
+
+        this.motor.activarCamara("camara1");
+
+        this.motor.activarLuz("luz1");
+
+
+
+        //plantas
+        this.motor.crearNodoMalla("maceta1", "maceta", undefined);
+        this.motor.crearNodoMalla("maceta2", "maceta", undefined);
+        for(let i=0; i<6; i++){
+            for(let j=0; j<4; j++){
+                this.motor.crearNodoMalla("calabaza"+i+"-"+j, "calabaza", undefined);
+                this.motor.escalarMalla("calabaza"+i+"-"+j, 3, 3, 3);
+                this.motor.moverMalla("calabaza"+i+"-"+j, -30+(i*10), -15, -20+(j*10));
+
+            }
+        }
+
+
+        this.motor.moverMalla("maceta1", 6, -18, -35);
+        this.motor.moverMalla("maceta2", -10, -18, -35);
+
+
+        //declaramos las variables necesarias para ejecutar el programa
+        //las variables de WebGL empezarán siempre por gl para distinguirlas de
+        //las variables del motor gráfico
+        window.glVertexShader=null;
+        window.glFragmentShader=null;
+        window.glProgram=null;
+
+
+        //CARLOS, TRATA DE ARRANCARLO (WEBGL)
+        this.motor.draw();
+  }
+
+
 }
---
+++ GardiotFrontEnd/src/assets/motor/TRecursoMaterial.js
@@ -0,0 +1,12 @@
+class TRecursoMaterial extends TRecurso{
+ /* var vertices, normales, texturas;
+  var vertTriangulos, normTriangulos, textTriangulos;
+  var nTriangulos;*/
+  constructor (nombre){
+    super(nombre);
+  }
+  cargarFichero(nombre){
+
+  }
+  // draw();
+}
