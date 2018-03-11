d6ca06e45488fda3052af6930cc69ae5945ede26
Julián Sánchez <eljovenjuli@hotmail.com>
2018-03-11 17:05:43 +0100

    resetPassPreAPI

--- GardiotFrontEnd/src/app/components/manage/reset-pass.component.ts
+++ GardiotFrontEnd/src/app/components/manage/reset-pass.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
 import { User } from "../../classes/user.class";
+import { AppComponent } from "../../app.component";
 @Component({
   selector: 'app-reset-pass',
   templateUrl: './reset-pass.component.html',
@@ -9,12 +10,16 @@ import { User } from "../../classes/user.class";
 export class ResetPassComponent implements OnInit {
 
   user=new User("");
-  constructor() {}
+  constructor(
+    private _appComponent:AppComponent,
+  ) {}
 
   resetPass(f: NgForm) {
-      var email = f.value;
-      var nombre = email.first;
-      console.log(nombre);
+      var valor = f.value;
+      var email = valor.first;
+      // LLAMADA A LA API
+      console.log(email);
+      this._appComponent.mensajeEmergente("Mensaje enviado. Revisa tu correo", "primary", "login");
     }
 
 
