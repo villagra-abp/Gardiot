4b32879b7b301ed6d926c91ebf67c441a179fb03
Julián Sánchez <eljovenjuli@hotmail.com>
2018-04-17 15:43:37 +0200

    AddSelectMinutesGrafana

--- GardiotFrontEnd/src/app/components/admin/statistics.component.ts
+++ GardiotFrontEnd/src/app/components/admin/statistics.component.ts
@@ -10,24 +10,30 @@ import { MatSelectModule } from '@angular/material/select';
 })
 export class StatisticsComponent implements OnInit {
   dialogResult = "";
+  selectedValue: string;
+
+  minutos = [
+    {value: '15', viewValue: 'Últimos 15 minutos'},
+    {value: '60', viewValue: 'Últimos 60 minutos'},
+    {value: '120', viewValue: 'Últimos 120 minutos'}
+  ];
   constructor( public dialog:MatDialog ) {
-    this.minutos=[15,60,120];
-   }
-   capturar(){
 
    }
+
 
   ngOnInit() {
   }
-  openDialog(){
-    let dialogRef = this.dialog.open(DialogDeleteComponent, {
-      width:'600px',
-      data: 'Hola Mriano'
-    });
-    dialogRef.afterClosed().subscribe(result=> {
-      console.log(`Dialog closed: ${result}`);
-      this.dialogResult = result;
-    })
-  }
+
+  // openDialog(){
+  //   let dialogRef = this.dialog.open(DialogDeleteComponent, {
+  //     width:'600px',
+  //     data: 'Hola Mriano'
+  //   });
+  //   dialogRef.afterClosed().subscribe(result=> {
+  //     console.log(`Dialog closed: ${result}`);
+  //     this.dialogResult = result;
+  //   })
+  // }
 
 }
