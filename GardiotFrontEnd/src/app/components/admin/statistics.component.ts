790e66e8c5607bbf79bf87b68ee1532d9d6509f0
Julián Sánchez <eljovenjuli@hotmail.com>
2018-04-17 16:35:42 +0200

    selectTimeGrafic

--- GardiotFrontEnd/src/app/components/admin/statistics.component.ts
+++ GardiotFrontEnd/src/app/components/admin/statistics.component.ts
@@ -15,14 +15,21 @@ export class StatisticsComponent implements OnInit {
   minutos = [
     {value: '15', viewValue: 'Últimos 15 minutos'},
     {value: '60', viewValue: 'Últimos 60 minutos'},
-    {value: '120', viewValue: 'Últimos 120 minutos'}
+    {value: '120', viewValue: 'Últimos 120 minutos'},
+    {value: '1440', viewValue: 'Últimas 24 horas'},
+    {value: '10080', viewValue: 'Últimos 7 días'},
+    {value: '44640', viewValue: 'Último mes'},
+    {value: '525600', viewValue: 'Último año'},
+    {value: '1051200', viewValue: 'Últimos dos años'},
   ];
   constructor( public dialog:MatDialog ) {
 
    }
 
 
+
   ngOnInit() {
+
   }
 
   // openDialog(){
