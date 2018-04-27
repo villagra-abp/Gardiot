9767840f71706214cf38250f138bfbe8eebb6a05
Julián Sánchez <eljovenjuli@hotmail.com>
2018-04-27 22:39:52 +0200

    AddResetPassByAdmin
    Todavia necesta comprobar que el usuario existe y validar el email en el formulario

--- GardiotFrontEnd/src/app/components/manage/reset-pass.component.ts
+++ GardiotFrontEnd/src/app/components/manage/reset-pass.component.ts
@@ -12,24 +12,52 @@ import { UserService } from "../../services/user.service";
 export class ResetPassComponent implements OnInit {
 
   // user=new User("");
+  public semaforo;
   public apiURL:string="https://gardiot.ovh/api/";
   constructor(
     private _appComponent:AppComponent,
     private _resetPassword:UserService,
+    private _isAdmin:UserService,
   ) {}
+
+  isAdmin(){
+    // LLAMADA A LA API
+    this._isAdmin.isUserAdmin()
+      .subscribe(data=>{
+        this.semaforo = data;
+        console.log("Es admin ? "+this.semaforo);
+    });
+  }
 
   resetPass(f: NgForm) {
       var valor = f.value;
       var email:String = valor.first;
 
-      // LLAMADA A LA API
-      this._resetPassword.resetPassword(email)
-        .subscribe(data=>{
-    });
-      this._appComponent.mensajeEmergente("Mensaje enviado. Revisa tu correo", "primary", "login");
+      // comprobar que el formulario no este vacio
+      if (valor.first != "") {
+      // comprobar que el usuario exista
+
+      // comprueba si el usuario es admin o no
+        if(this.semaforo){ // bool que comprueba si es admin
+          // LLAMADA A LA API
+          this._resetPassword.resetPassword(email)
+            .subscribe(data=>{
+        });
+          this._appComponent.mensajeEmergente("Mensaje enviado al usuario", "primary", "admin/edituser/"+email); /*/admin/edituser/   admin/users?pag=1 */
+        }else{
+          // console.log("NO soy admin");
+          // LLAMADA A LA API
+          this._resetPassword.resetPassword(email)
+            .subscribe(data=>{
+        });
+          this._appComponent.mensajeEmergente("Mensaje enviado. Revisa tu correo", "primary", "login");
+        }
+      }else{
+        this._appComponent.mensajeEmergente("Introduce un email.", "danger", "");
+      }
     }
 
   ngOnInit() {
+    this.isAdmin();
   }
-
 }
