90d66f252cceecb0a43854323f49f6b92daff148
Luis Berenguer Herrero <luisberenguer96@gmail.com>
2018-05-11 17:58:37 +0200

    Fix

--- GardiotFrontEnd/src/app/services/plant.service.ts
+++ GardiotFrontEnd/src/app/services/plant.service.ts
@@ -16,6 +16,9 @@ export class PlantService {
 		else if (window.location.toString().indexOf("gardiot") >= 0) {
 			this.apiURL = "https://gardiot.ovh/api/";
 		}
+		else{
+			this.apiURL = "http://192.168.100.3:3000/api/";
+		  }
 	}
 
 	save(plant: Plant) {
