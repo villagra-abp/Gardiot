83a939f6ff02a5912477fb23ea313e28d7543d15
Julián Sánchez <eljovenjuli@hotmail.com>
2018-05-22 13:24:47 +0200

    FixResetPassAndPassBack

--- GardiotFrontEnd/src/app/components/manage/reset-pass.component.ts
+++ GardiotFrontEnd/src/app/components/manage/reset-pass.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
-// import { User } from "../../classes/user.class";
 import { AppComponent } from "../../app.component";
 import { UserService } from "../../services/user.service";
 
@@ -11,67 +10,35 @@ import { UserService } from "../../services/user.service";
 })
 export class ResetPassComponent implements OnInit {
 
-  // user=new User("");
   public semaforo;
   public correo;
   public apiURL:string="https://gardiot.ovh/api/";
   constructor(
     public _appComponent:AppComponent,
     public _resetPassword:UserService,
-    public _isAdmin:UserService,
-    public _userExist:UserService,
-  ) {}
 
-  isAdmin(){
-    // LLAMADA A LA API
-    this._isAdmin.isUserAdmin()
-      .subscribe(data=>{
-        this.semaforo = data;
-        console.log("Es admin ? "+this.semaforo);
-    });
-  }
+  ) {}
 
   resetPass(f: NgForm) {
       var valor = f.value;
       var email:String = valor.first;
       // comprobar que el formulario no este vacio
       if (valor.first != "") {
-        // comprobar que el usuario exista
-        this._userExist.isUser(email)
+      // LLAMADA A LA API
+        this._resetPassword.resetPassword(email)
           .subscribe(data=>{
-            this.correo=data[0].id;
-          });
-
-          if(this.correo != undefined){
-            // comprueba si el usuario es admin o no
-              if(this.semaforo){ // bool que comprueba si es admin
-                // LLAMADA A LA API
-                this._resetPassword.resetPassword(email)
-                  .subscribe(data=>{
-              });
-                this._appComponent.mensajeEmergente("Mensaje enviado al usuario", "primary", "admin/edituser/"+email); /*/admin/edituser/   admin/users?pag=1 */
-              }else{
-                // console.log("NO soy admin");
-                // LLAMADA A LA API
-                this._resetPassword.resetPassword(email)
-                  .subscribe(data=>{
-              });
-                this._appComponent.mensajeEmergente("Mensaje enviado. Revisa tu correo", "primary", "login");
-              }
-
-          }else{
-            this._appComponent.mensajeEmergente("No existe el usuario", "danger", "");
-
-          }
-
-        }
-
-        else{
+            this._appComponent.mensajeEmergente(data.Mensaje, "primary", "login/");
+        },
+        error => {
+          let v = JSON.parse(error._body);
+          this._appComponent.mensajeEmergente(v.Mensaje, "danger", "");
+        });
+        }else{
           this._appComponent.mensajeEmergente("Introduce un email.", "danger", "");
       }
     }
-
+    
   ngOnInit() {
-    this.isAdmin();
+
   }
 }
